6d4b4c13c7aa665bc2d74a6b7c6b8b9432011f1c
Five-great <43601963+Five-great@users.noreply.github.com>
2022-04-12 08:55:04 +0000

    update guide/quickStart.md.

---
+++ assets/index.3fd767d5.js
@@ -0,0 +1,63 @@
+import"./index.79eb138f.js";import"./tpImportword.3f709111.js";import{T as u}from"./Tinymce-vue.1480c1d1.js";import"./tp-zh_CN.e06c06f3.js";import{_ as b,r as m,c as g,b as r,a as c,p as f,d as _,o as v}from"./index.c248884f.js";const x={components:{TinymceVue:u},name:"vueDemo",data(){return{editor:"",content:"dsdsdsdsd",content2:`<div class="tp-tabs" style="width: 100%;" data-id="tp$tabsID1629518201020" data-tp-component="tabs"><input id="tp$tabsID1629518201020tab0" checked="" name="tp$tabsID1629518201020" type="radio" /><input id="tp$tabsID1629518201020tab1" name="tp$tabsID1629518201020" type="radio" />
+<div class="tp-tabs_top"><label class="tp-tabs_label" for="tp$tabsID1629518201020tab0">Title {1}</label><label class="tp-tabs_label" for="tp$tabsID1629518201020tab1">Title {1}</label></div>
+<div class="tp-tabs_main">
+<div class="tp-tab_main">Write here 1</div>
+<div class="tp-tab_main">Write here 2</div>
+</div>
+</div>
+<p style="margin-top: 0px;">\u5076u\u90FD\u5F88\u7B26\u5408\u901F\u5EA6\u53D1\u8D27\u901F\u5EA6v\u73B0\u573Av\u73B0\u573A\u5C31\u4F1A\u53D1\u89C9\u5361\u6B7B\u7684\u56DE\u590D\u51E0\u4E4E\u90FD\u662F\u5F00\u53D1&nbsp;</p>
+<p style="margin-top: 0px;">\u554A\u5B9E\u6253\u5B9E\u963F\u677E\u5927\u6328\u6253\u6328\u6253\u6328\u6253</p>
+<a class="tp-buttons" style="margin: 0 auto;" href="http://127.0.0.1:3000/examples/" data-tp-style="style_2" data-id="tp-buttonsID1631194286931" data-tp-component="buttons">213dsfsdf132</a>
+<ul class=" tp-iconlists tp-iconlists_tick" style="list-style-type: tp-iconlists_tick;">
+<li><span style="font-size: 16px;">\u7684\u662F\u5426\u4F1A\u8212\u670D\u7B2C\u56DBu\u5BF9\u4ED8i\u6DD1\u5983\u4E3A\u66F4\u597D\u8BA4\u4E3A\u4EBAu\u7231\u4E0A\u5BF9\u554A\u56DE\u590D\u554A\u662F\u5426\u901F\u5EA6\u6CD5\u53CC\u65B9\u7684\u7684\u987A\u4E30\u5230\u4ED8\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5 \u7684\u62A4\u8EAB\u7B26i\u8FD8\u662F\u77ED\u53D1</span></li>
+<li><span style="font-size: 16px;">\u58EB\u5927\u592B\u7CBE\u795E\u7684\u56DE\u590D\u54C8\u5E02\u5927\u5BB6\u53D1\u8D27\u7684\u5065\u8EAB\u623F</span></li>
+</ul>
+<p style="margin-top: 0px;">&rsquo;</p>
+<table style="border-collapse: collapse; width: 99.8301%; height: 466px;" border="1">
+<tbody>
+<tr style="height: 22px;">
+<td style="width: 13.0997%; height: 22px;">&nbsp;</td>
+<td style="width: 13.0997%; height: 22px;">\u58EB\u5927\u592B\u6492\u65E6</td>
+<td style="width: 13.0997%; height: 22px;">&nbsp;</td>
+<td style="width: 13.0997%; height: 22px;">&nbsp;</td>
+<td style="width: 13.0997%; height: 22px;">&nbsp;</td>
+<td style="width: 13.0997%; height: 22px;">&nbsp;</td>
+<td style="width: 13.1064%; height: 22px;">&nbsp;</td>
+</tr>
+<tr style="height: 422px;">
+<td style="width: 13.0997%; height: 422px;">\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5</td>
+<td style="width: 13.0997%; height: 422px;">\u5012\u662F\u53D1\u751F\u7684f</td>
+<td style="width: 13.0997%; height: 422px;">\u901F\u5EA6\u6CD5\u7684\u6492\u6CD5</td>
+<td style="width: 13.0997%; height: 422px;">\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5\u8212\u670D</td>
+<td style="width: 13.0997%; height: 422px;">sd\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5</td>
+<td style="width: 13.0997%; height: 422px;">\u5730\u65B9\u901F\u5EA6\u6CD5</td>
+<td style="width: 13.1064%; height: 422px;">\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5</td>
+</tr>
+<tr style="height: 22px;">
+<td style="width: 13.0997%; height: 22px;">\u5012\u662F\u8985\u7B2C\u4E09\u65B9\u901F\u5EA6\u6CD5</td>
+<td style="width: 13.0997%; height: 22px;">\u7684\u6492\u6CD5\u7684\u6492\u6CD5</td>
+<td style="width: 13.0997%; height: 22px;">\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5sd</td>
+<td style="width: 13.0997%; height: 22px;">\u6492\u7684\u8985\u7B2C\u4E09\u65B9</td>
+<td style="width: 13.0997%; height: 22px;">\u58EB\u5927\u592B\u6492\u65E6\u7684\u6492\u6CD5sdf</td>
+<td style="width: 13.0997%; height: 22px;">\u7684\u6492\u6CD5\u901F\u5EA6\u6CD5</td>
+<td style="width: 13.1064%; height: 22px;">\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5</td>
+</tr>
+</tbody>
+</table>
+<p style="margin-top: 0px;">\u7684\u53D1\u6325\u90B8</p>
+<div class="tp-tabs" style="width: 99.50%; margin: 0 auto;" data-id="tp$tabsID1631194293489" data-tp-component="tabs"><input id="tp$tabsID1631194293489tab0" checked="" name="tp$tabsID1631194293489" type="radio" /><input id="tp$tabsID1631194293489tab1" name="tp$tabsID1631194293489" type="radio" />
+<div class="tp-tabs_top"><label class="tp-tabs_label" for="tp$tabsID1631194293489tab0">\u6807\u9898 1</label><label class="tp-tabs_label" for="tp$tabsID1631194293489tab1">\u6807\u9898 2</label></div>
+<div class="tp-tabs_main">
+<div class="tp-tab_main">Write here 1</div>
+<div class="tp-tab_main">Write here 2</div>
+</div>
+</div>
+<p style="margin-top: 0px;">\u7684\u6492\u53D1\u5C04\u70B9\u6839\u6DF1\u8482\u56FA\u53D1\u58EB\u5927\u592B\u901F\u5EA6\u6CD5\u7684\u6492\u6CD5\u6492\u7684\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5\u901F\u5EA6\u6CD5\u7684\u6492\u6CD5</p>
+<div class="tp-collapse" style="width: 99.50%; margin: 0 auto;" data-top-style="padding:1px 40px;" data-template="default" data-tp-component="collapse"><input id="tp$collapseID1631194297628" type="checkbox" /><label class="tp-collapse_label" style="min-height: 20px; padding: 1px 40px;" for="tp$collapseID1631194297628">
+<p>\u6807\u9898</p>
+</label>
+<div class="tp-collapse_main">
+<p>\u5728\u8FD9\u91CC\u5199\u5165\u5185\u5BB9</p>
+</div>
+</div>
+<p style="margin-top: 0px;">\u554A\u58EB\u5927\u592B\u8270\u82E6\u54C8\u6570\u636E\u8FD4\u56DE\u7684\u662F\u4F0F\u864E\u7684\u723D\u80A4\u6C34u\u554A\u58EB\u5927\u592B\u5580\u4EC0\u80AF\u5FB7\u57FA\u54C8\u8428\u514B\u63A5\u7535\u8BDD\u770B\u6492\u53EF\u89C1\u5EA6\u8D3A\u5361\u6536\u5230\u5C3D\u5FEB\u54C8\u6492\u53EF\u89C1\u5EA6v\u5427\u554A\u5C31\u662F\u8D37\u8BB0\u5361\u548C\u6570\u636E\u5E93\u5927\u82CF\u6253 kjahscjkhaxjzcb\u963F\u65AF\u8482\u82AC\u62C9\u8428xcasdfdsfdsf</p>`,vueDemoOpt:"",vueDemoOpt2:{min_height:200,max_height:700,skeletonScreen:!0,plugins:"tp code  tpIndent2em tpLetterspacing autoresize tpCollapse tpTabs tpButtons lists tpIconlists  tpColumns tpParagraph  tpIconfont tpImportword preview",toolbar:["code formatselect fontselect  fontsizeselect  forecolor backcolor bold italic underline strikethrough tpIndent2em tpLetterspacing tpImportword  tpCollapse tpTabs tpButtons tpIconlists  tpColumns tpParagraph tpIconfont | Preview"]}}},methods:{important(){tinyMCE.activeEditor.execCommand("mceImportword")},init(){let s=this;s.vueDemoOpt={toolbar_groups:{formatting:{text:"\u6587\u5B57\u683C\u5F0F",tooltip:"Formatting",items:"bold italic underline | superscript subscript"},alignment:{icon:"align-left",tooltip:"alignment",items:"alignleft aligncenter alignright alignjustify"}},extend_groups_addicon:{mygroupsicon:'<img  src="https://avatars.githubusercontent.com/u/87648636?s=60&v=4" style="width:20px;" >'},extend_groups:{mygroups:{icon:"mygroupsicon",tooltip:"mygroupsicon",isSelect:!0,type:"togglemenuitem",items:[{type:"selectItem",text:"\u5B57\u4F53",value:"12px 14px 16px 18px 24px 36px 48px 56px 72px",default:"16px",styleSelector:"font-size",onAction:function(e,l){e.formatter.apply("fontsize",{value:l})}},{icon:"underline",text:"\u4E0B\u5212\u7EBF",value:"underline",styleSelector:"text-decoration"},{icon:"bold",text:"\u52A0\u7C97",value:"bold",selector:"strong"},{icon:"italic",text:"\u659C\u4F53",value:"italic",selector:"em"}]}},plugins:"print tp preview extendgroups clearhtml searchreplace  insertdatetime autolink layout fullscreen line-height image imagetools media upfile link   autosave code  table  advlist lists checklist hr emoticons autosave bdmap indent2em   axupimgs  letterspacing  quickbars attachment wordcount template  autoresize importword searchreplace pagebreak pageembed  tpCollapse tpTabs tpButtons tpIconlists",toolbar:["code formatselect fontselect  fontsizeselect   forecolor backcolor bold italic underline strikethrough link alignment alignmentdrop undo redo  restoredraft layout upfile importword hr lineheight letterspacing line-height indent2em table bdmap image media attachment outdent indent blockquote subscript superscript  emoticons mygroups  preview searchreplace","pagebreak template pageembed bullist numlist checklist tpCollapse tpTabs tpButtons tpIconlists"],branding:!1,menubar:!1,language:"zh_CN",schema:"html5",min_height:400,max_height:700,template_replace_values:{username:"Jack Black",staffid:"991234",inboth_username:"Famous Person",inboth_staffid:"2213"},template_preview_replace_values:{preview_username:"Jack Black",preview_staffid:"991234",inboth_username:"Famous Person",inboth_staffid:"2213"},templates:[{title:"Date modified example",description:"Adds a timestamp indicating the last time the document modified.",content:'<p>Last Modified: <time class="mdate">This will be replaced with the date modified.</time></p>'},{title:"Replace values example",description:"These values will be replaced when the template is inserted into the editor content.",content:"<p>Name: {$username}, StaffID: {$staffid}</p>"},{title:"Replace values preview example",description:"These values are replaced in the preview, but not when inserted into the editor content.",content:"<p>Name: {$preview_username}, StaffID: {$preview_staffid}</p>"},{title:"Replace values preview and content example",description:"These values are replaced in the preview, and in the content.",content:"<p>Name: {$inboth_username}, StaffID: {$inboth_staffid}</p>"}],table_default_attributes:{border:"1"},table_default_styles:{"border-collapse":"collapse",width:"100%"},table_header_type:"sectionCells",table_responsive_width:!0,skeletonScreen:!0,images_upload_handler:function(e,l,p,t){var n=e.blob(),a=new FileReader,o="";a.onload=function(d){o=d.target.result};var i=new FormData;i.append("file",n),s.$http({data:i,type:"GET",url:"/tinymce/api/file.json",header:{"Content-Type":"multipart/form-data"},onUploadProgress(d){t(d+"%")}}).then(function(d){d.code==200?l(o):p("\u4E0A\u4F20\u5931\u8D25:"+i.data)}).catch(function(d){p("\u4E0A\u4F20\u5931\u8D25:"+d.message)}),a.readAsDataURL(n)},file_picker_callback:function(e,l,p){var t=".pdf, .txt, .zip, .rar, .7z, .doc, .docx, .xls, .xlsx, .ppt, .pptx, .mp3, .mp4",n=document.createElement("input");n.setAttribute("type","file"),n.setAttribute("accept",t),n.click(),n.onchange=function(){var a=this.files[0],o=new FormData;o.append("file",a),s.$http.get({data:o,url:"./api/file.json",header:{"Content-Type":"multipart/form-data"},xhr:s.xhrOnProgress(function(i){const d=(i.loaded/i.total*100|0)+"%";progressCallback(d)})}).then(i=>{e(i.data,{text:i.data})}).catch(function(i){failFun("\u4E0A\u4F20\u5931\u8D25:"+i.message)})}},file_callback:function(e,l){var p=new FormData;p.append("file",e),console.log(e),s.$http({data:p,type:"GET",url:"/tinymce/api/file.json",header:{"Content-Type":"multipart/form-data"}}).then(function(t){console.log(t),t.code==200&&l(t.data,{text:e.name})}).catch(function(t){})},tp_attachment_assets_path:"./plugins/attachment/icons",tp_attachment_icons_path:"https://unpkg.com/@npkg/tinymce-plugins/plugins/attachment/icons",tp_attachment_upload_handler:function(e,l,p,t){var n=new FormData;n.append("file",e),s.$http({data:n,type:"GET",url:"/tinymce/api/file.json",header:{"Content-Type":"multipart/form-data"},xhr:s.xhrOnProgress(function(a){const o=(a.loaded/a.total*100|0)+"%";t(o)})}).then(function(a){a.code==200?l(a.data):p("\u4E0A\u4F20\u5931\u8D25:"+a.data)}).catch(function(a){p("\u4E0A\u4F20\u5931\u8D25:"+a.message)})},attachment_max_size:5009715200}}},created(){this.init()}},h=s=>(f("data-v-6699c56e"),s=s(),_(),s),y={class:"vueDemo2"},w=h(()=>r("h1",null,"\u5C55\u793A\u663E\u793A2",-1)),I=["innerHTML"],k=h(()=>r("h1",null,"\u63D2\u4EF6demo\u5C55\u793A\u533A\u57DF",-1)),D={class:"vueDemo"},T=h(()=>r("h1",null,"\u5C55\u793A\u663E\u793A",-1)),$=["innerHTML"];function C(s,e,l,p,t,n){const a=m("tinymce-vue"),o=m("PagesRouter");return v(),g("div",null,[r("div",y,[c(a,{modelValue:t.content2,"onUpdate:modelValue":e[0]||(e[0]=i=>t.content2=i),options:t.vueDemoOpt2},null,8,["modelValue","options"])]),w,r("div",{innerHTML:t.content2},null,8,I),k,r("div",D,[c(a,{modelValue:t.content,"onUpdate:modelValue":e[1]||(e[1]=i=>t.content=i),options:t.vueDemoOpt},null,8,["modelValue","options"])]),T,r("div",{innerHTML:t.content},null,8,$),c(o,{pagesName:"vuedemo"})])}var j=b(x,[["render",C],["__scopeId","data-v-6699c56e"]]);export{j as default};
