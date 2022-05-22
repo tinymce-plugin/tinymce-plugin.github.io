a49671f10b2883ffff5077b8b54a0573eb3fcdf1
Five-great <43601963+Five-great@users.noreply.github.com>
2022-05-22 05:48:55 +0000

    Update skeleton.md

---
+++ assets/chunk/skeleton_t_n8sxcz1653198451072_p_.js
@@ -0,0 +1,2 @@
+import"./pinia.js";import{J as o}from"./vue.js";const s=o({components:{},template:`<div class="fv-mardown-html"><div class="fv-mardown-main"><p>404: Not Found</p>
+</div></div><PagesRouter  docPath="guide/skill/skeleton.md" mapType="docs" docRepo="tinymce-plugin-docs" pagesName="skeleton_t_n8sxcz1653198451072_p_" />`});export{s as default};
