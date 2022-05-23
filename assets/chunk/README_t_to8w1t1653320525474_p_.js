96cd6e5125f4c6f5d1ea6dec64d119dcd4f042b0
Five-great <43601963+Five-great@users.noreply.github.com>
2022-05-23 15:43:39 +0000

    test

---
+++ assets/chunk/README_t_to8w1t1653320525474_p_.js
@@ -0,0 +1,3 @@
+import"./pinia.js";import{J as t}from"./vue.js";const e=t({components:{},template:`<div class="fv-mardown-html"><div class="fv-mardown-main"><h1 id="tp-layout" tabindex="-1"><a class="header-anchor" href="#tp-layout">#</a> tp-layout</h1>
+<p>One click layout</p>
+</div></div><PagesRouter  docPath="README.md" mapType="docs" docRepo="tp-layout" pagesName="README_t_to8w1t1653320525474_p_" />`});export{e as default};
---
+++ assets/chunk/tpIndent2em_t_vciccf1653320525474_p_.js
@@ -0,0 +1,2 @@
+import"./pinia.js";import{J as t}from"./vue.js";const p=t({components:{},template:`<div class="fv-mardown-html"><div class="fv-mardown-main"><p>#test</p>
+</div></div><PagesRouter  docPath="__docs__/tpIndent2em.md" mapType="docs" docRepo="tp-indent2em" pagesName="tpIndent2em_t_vciccf1653320525474_p_" />`});export{p as default};
