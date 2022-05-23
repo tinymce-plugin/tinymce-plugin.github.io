fbfcf34e606f1bab0f00d702a9221c7169dea323
Five-great <43601963+Five-great@users.noreply.github.com>
2022-05-23 16:12:40 +0000

    test

--- assets/chunk/README_t_6v9kmp1653321182110_p_.js
+++
@@ -1,3 +0,0 @@
-import"./pinia.js";import{J as a}from"./vue.js";const e=a({components:{},template:`<div class="fv-mardown-html"><div class="fv-mardown-main"><h1 id="tp-layout" tabindex="-1"><a class="header-anchor" href="#tp-layout">#</a> tp-layout</h1>
-<p>One click layout</p>
-</div></div><PagesRouter  docPath="README.md" mapType="docs" docRepo="tp-layout" pagesName="README_t_6v9kmp1653321182110_p_" />`});export{e as default};
