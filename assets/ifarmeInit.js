80bc31d4df6a199c481bd9fa6a219c2e213f3320
Five-great <43601963+Five-great@users.noreply.github.com>
2022-06-01 17:58:18 +0000

    Update skeleton.md

---
+++ assets/ifarmeInit.js
@@ -0,0 +1,11 @@
+
+const autoIframeHeight = (that)=>{
+
+  that.contentWindow.TimeID = that.contentWindow.setInterval(()=>{
+      if(that&&that.contentWindow&&that.contentWindow.document){
+        that.height = that.contentWindow.document.body.scrollHeight+25
+      }else{
+        that.contentWindow.clearInterval(that.contentWindow.TimeID)
+      }
+  },100)
+}
