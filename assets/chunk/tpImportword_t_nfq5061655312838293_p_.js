6fb813b46fb35025d3ba1b2c48f54c537bced567
Five-great <43601963+Five-great@users.noreply.github.com>
2022-06-15 17:09:17 +0000

    Merge branch 'main' of https://github.com/tinymce-plugin/tinymce-plugin-docs

---
+++ assets/chunk/tpImportword_t_nfq5061655312838293_p_.js
@@ -0,0 +1,2 @@
+import"./pinia.js";import{M as o}from"./vue.js";const d=o({components:{},template:`<div class="fv-mardown-html"><div class="fv-mardown-main tp-doc"><h1 id="tpImportword" tabindex="-1"><a class="header-anchor" href="#tpImportword">#</a> tpImportword</h1>
+</div></div><PagesRouter  docPath="__docs__/tpImportword.md" mapType="docs" docRepo="tp-importword" pagesName="tpImportword_t_nfq5061655312838293_p_" />`});export{d as default};
