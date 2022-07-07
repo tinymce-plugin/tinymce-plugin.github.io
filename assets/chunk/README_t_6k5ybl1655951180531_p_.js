2f5effa83b9107b01ccd2c7b8112d1e18983ce77
Five-great <43601963+Five-great@users.noreply.github.com>
2022-07-07 12:12:57 +0000

    0.0.3-beta.4 form @ tinymce-plugin/tp-importword@ec006efad3fc98d3c80af94203804292d9b64dbc 🚀

--- assets/chunk/README_t_6k5ybl1655951180531_p_.js
+++
@@ -1,7 +0,0 @@
-import "./pinia.js";
-import { M as defineComponent } from "./vue.js";
-const __script = defineComponent({
-  components: {},
-  template: '<div class="fv-mardown-html"><div class="fv-mardown-main tp-doc"><h1 id="tp-layout" tabindex="-1"><a class="header-anchor" href="#tp-layout">#</a> tp-layout</h1>\n<p>One click layout</p>\n</div></div><PagesRouter  docPath="README.md" mapType="docs" docRepo="tp-layout" pagesName="README_t_6k5ybl1655951180531_p_" />'
-});
-export { __script as default };
--- assets/chunk/grammar_t_cmax0y1655951180531_p_.js
+++
@@ -1,7 +0,0 @@
-import "./pinia.js";
-import { M as defineComponent } from "./vue.js";
-const __script = defineComponent({
-  components: {},
-  template: '<div class="fv-mardown-html"><div class="fv-mardown-main tp-doc"><h1 id="\u6587\u6863\u8BED\u6CD5" tabindex="-1"><a class="header-anchor" href="#\u6587\u6863\u8BED\u6CD5">#</a> \u6587\u6863\u8BED\u6CD5</h1>\n</div></div><PagesRouter  docPath="guide/contributing/writing-guide/grammar.md" mapType="docs" docRepo="tinymce-plugin-docs" pagesName="grammar_t_cmax0y1655951180531_p_" />'
-});
-export { __script as default };
