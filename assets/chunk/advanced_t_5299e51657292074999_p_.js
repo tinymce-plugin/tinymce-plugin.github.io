07fb4c1b2cc0e6def499b31397caae840cacf5ac
five <fivecc@qq.com>
2022-07-08 23:00:24 +0800

    i18n

---
+++ assets/chunk/advanced_t_5299e51657292074999_p_.js
@@ -0,0 +1,5 @@
+import"./pinia.js";import{M as e}from"./vue.js";const n=e({components:{},template:`<div class="fv-mardown-html"><div class="fv-mardown-main tp-doc"><h2 id="\u9AD8\u9636\u4F7F\u7528" tabindex="-1"><a class="header-anchor" href="#\u9AD8\u9636\u4F7F\u7528">#</a> \u9AD8\u9636\u4F7F\u7528</h2>
+<ul>
+<li>[x] test</li>
+</ul>
+</div></div><PagesRouter  docPath="guide/examples/advanced.md" mapType="docs" docRepo="tinymce-plugin-docs" pagesName="advanced_t_5299e51657292074999_p_" />`});export{n as default};
